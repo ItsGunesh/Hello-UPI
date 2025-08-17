276c4defe4347acb13d7282fb9e34a18a86e8138
ItsGunesh <itsme.gunesh87@gmail.com>
2025-08-17 17:54:47 +0530

    Home navigate

--- Frontend/components/Navigator/Navigator.jsx
+++ Frontend/components/Navigator/Navigator.jsx
@@ -1,15 +1,20 @@
 import React, { useEffect, useState } from 'react'
-// import axios from 'axios'
-// import { useNavigate } from 'react-router-dom'
-// import { faBars } from '@fortawesome/free-solid-svg-icons'
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useNavigate } from 'react-router-dom'
 
 const Navigator = () => {
+
+  const navigate = useNavigate()
+
+  const toHome = ()=>{
+    navigate("/")
+  }
+
+
   return (
     <>
       <div className='flex justify-between shadow-md box-border w-full py-2 px-6 bg-gradient-to-r from-slate-100 via-slate-100 to-slate-100'>
         <div>
-          <p className='test-font text-3xl'>Hello UPI</p>
+          <p className='test-font text-3xl' onClick={toHome}>Hello UPI</p>
           <p>{Date().toString}</p>
         </div>
         <div className='flex items-center gap-20'>
