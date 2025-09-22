f68a1e6443ed7c2814772206b2881e1fc891ac52
ItsGunesh <itsme.gunesh87@gmail.com>
2025-09-22 17:05:57 +0530

    Gemini API added - Tested for Hindi

--- Frontend/components/VoiceHandler.jsx
+++ Frontend/components/VoiceHandler.jsx
@@ -10,11 +10,12 @@ const VoiceHandler = ({ onCommand }) => {
   const [isProcessing, setIsProcessing] = useState(false);
   const [pinAttempts, setPinAttempts] = useState(0);
   const [shouldClearPin, setShouldClearPin] = useState(false);
-  const [pinstatus,setPinStatus] = useState('')
+  const [pinstatus, setPinStatus] = useState('')
 
-  
+
   const CORRECT_PIN = "2308";
-  const MAX_PIN_ATTEMPTS = 3; 
+  const MAX_PIN_ATTEMPTS = 3;
+
 
 
   const processPayment = async (amount, person) => {
@@ -23,12 +24,12 @@ const VoiceHandler = ({ onCommand }) => {
 
     try {
       // console.log(apiUrl)
-      
+
       const requestData = {
         person: person,
         amount: amount
       }
-      
+
       const response = await axios.post(`${apiUrl}/api/payment/processpayment`, requestData, {
         headers: {
           'Content-Type': 'application/json'
@@ -58,22 +59,22 @@ const VoiceHandler = ({ onCommand }) => {
         processPayment(pendingPayment.amount, pendingPayment.person);
         setPendingPayment(null);
       }
-          } else {
-        const newAttempts = pinAttempts + 1;
-        setPinAttempts(newAttempts);
-        
-        if (newAttempts >= MAX_PIN_ATTEMPTS) {
-          setShowPinModal(false);
-          setPinAttempts(0);
-          setPendingPayment(null);
-          onCommand("Too many failed PIN attempts. Payment cancelled.");
-        } else {
-          // onCommand(`Incorrect PIN. ${MAX_PIN_ATTEMPTS - newAttempts} attempts remaining.`);
-          setPinStatus("Incorrect PIN")
-          setShouldClearPin(true);
-          setTimeout(() => setShouldClearPin(false), 100);
-        }
+    } else {
+      const newAttempts = pinAttempts + 1;
+      setPinAttempts(newAttempts);
+
+      if (newAttempts >= MAX_PIN_ATTEMPTS) {
+        setShowPinModal(false);
+        setPinAttempts(0);
+        setPendingPayment(null);
+        onCommand("Too many failed PIN attempts. Payment cancelled.");
+      } else {
+        // onCommand(`Incorrect PIN. ${MAX_PIN_ATTEMPTS - newAttempts} attempts remaining.`);
+        setPinStatus("Incorrect PIN")
+        setShouldClearPin(true);
+        setTimeout(() => setShouldClearPin(false), 100);
       }
+    }
   };
 
   const handlePinClose = () => {
@@ -92,34 +93,34 @@ const VoiceHandler = ({ onCommand }) => {
       onCommand("Please complete the current transaction first.");
       return;
     }
-    
+
     setPendingPayment({ amount, person });
     setPinAttempts(0);
     setShowPinModal(true);
     onCommand(`Please enter PIN to send ₹${amount} to ${person}`);
   };
 
-  const handletranscript = async(sentence)=>{
+  const handletranscript = async (sentence) => {
 
     try {
 
       const apiUrl = import.meta.env.VITE_BACKEND_URL
 
-      const response = await axios.post(`${apiUrl}/api/pyserver/extract`,{sentence}, {
+      const response = await axios.post(`${apiUrl}/api/pyserver/extract`, { sentence }, {
         headers: {
           'Content-Type': 'application/json'
         },
         withCredentials: true
       })
 
-      if(response.status===200){
+      if (response.status === 200) {
         return response.data
       }
 
     } catch (error) {
-      console.log("Frontend handletranscript error :",error)
+      console.log("Frontend handletranscript error :", error)
     }
-      
+
   }
 
   useEffect(() => {
@@ -135,7 +136,7 @@ const VoiceHandler = ({ onCommand }) => {
     recognition.continuous = true;
     recognition.lang = "en-IN";
 
-    recognition.onresult = async(event) => {
+    recognition.onresult = async (event) => {
       const transcript = event.results[event.results.length - 1][0].transcript
         .trim()
         .toLowerCase();
@@ -144,34 +145,79 @@ const VoiceHandler = ({ onCommand }) => {
 
       if (transcript.includes("hello upi")) {
         onCommand("UPI activated.");
-      } else{
+      } else {
         onCommand(`Transaction command detected: "${transcript}"`);
 
-        const extractedtranscript = await handletranscript(transcript)
+        const prompt = `Convert this to english and return the sentence - ${transcript}`
+
+        const geminiApi = import.meta.env.VITE_GEMINI_API_KEY
+
+
+        const translatedText = await axios.post("https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent", {
+          contents: [
+            {
+              parts: [
+                {
+                  text: prompt
+                }
+              ]
+            }
+          ]
+        },
+          {
+            headers: {
+              'Content-Type': 'application/json',
+              'X-goog-api-key': geminiApi
+            },
+          })
+
+
+        if (translatedText.status === 200) {
+          // console.log("inside gemini response")
+          const translatedTextResponse = translatedText.data.candidates[0].content.parts[0].text
+
+          const translatedString =JSON.stringify(translatedTextResponse).slice(1,-4)
+          
+          // console.log(translatedTextResponse)
+          console.log(translatedString)
+          // console.log(typeof(translatedString))
+          const extractedtranscript = await handletranscript(translatedString)
+
+
+          // JSON.stringify(extractedtranscript)
+
+          const extractedAmount = extractedtranscript.data.Amount;
+          const extractedPerson = extractedtranscript.data.Receiver;
+
+          // console.log(extractedAmount)
+          // console.log(extractedPerson)
+
+          if (!extractedAmount || !extractedPerson) {
+            onCommand(extractedtranscript.data)
+          }
+          else {
+            initiatePayment(extractedAmount, extractedPerson)
+          }
+        }
+
 
         // console.log(extractedtranscript.data) .
-        
+
         // console.log(typeof(extractedtranscript.data))
 
-        const words = transcript.toLowerCase().split(' ');
-        const amountIndex = words.findIndex(word => word === 'send') + 1;
-        const toIndex = words.findIndex(word => word === 'to') + 1;
+        // const words = transcript.toLowerCase().split(' ');
+        // const amountIndex = words.findIndex(word => word === 'send') + 1;
+        // const toIndex = words.findIndex(word => word === 'to') + 1;
 
-        const extractedAmount = extractedtranscript.data.Amount;
-        const extractedPerson = extractedtranscript.data.Receiver;
+
 
         // console.log(extractedAmount)
         // console.log(extractedPerson)
 
-        if(!extractedAmount || !extractedPerson){
-          onCommand(extractedtranscript.data)
-        }
-        else{
-          initiatePayment(extractedAmount,extractedPerson)
-        }
 
 
-        
+
+
       }
     };
 
